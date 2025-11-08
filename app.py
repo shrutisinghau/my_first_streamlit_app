0ec6462d7863700500a7607daad093bb5ba90cb3
Shruti Singh (Cenitex) <shruti.singh@cenitex.vic.gov.au>
2025-11-08 19:00:38 +1100

    updated latest code

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ from snowflake.snowpark import Session
 import pandas as pd
 
 st.title("❄️ Streamlit in Snowflake Demo App")
+from datetime import datetime
+print(f"File updated successfully at {datetime.now()}")
+
 
